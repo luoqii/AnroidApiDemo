e53e7e24356f7584d309aae0ac4d6af3e14d8a26
宋帮远 <songbangyuan@bbk.com>
2021-01-11 14:50:10 +0800

    install nrf logger apk.

--- app/src/main/java/com/bysong/android/apidemo/MainActivity.java
+++ app/src/main/java/com/bysong/android/apidemo/MainActivity.java
@@ -1,6 +1,14 @@
 package com.bysong.android.apidemo;
 
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.ResourceUtils;
+import com.blankj.utilcode.util.Utils;
 
 import org.bbs.android.commonlib.activity.BaseApiDemo;
 
@@ -19,4 +27,5 @@ public class MainActivity extends BaseApiDemo {
     protected List<Map<String, Object>> getData(String prefix) {
         return super.getData(prefix);
     }
+
 }
--- app/src/main/java/x/android/app/Activity/Activity_Activity.java
+++ app/src/main/java/x/android/app/Activity/Activity_Activity.java
@@ -1,15 +1,23 @@
 package x.android.app.Activity;
 
 import androidx.annotation.RequiresApi;
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.ResourceUtils;
+import com.blankj.utilcode.util.Utils;
 import com.bysong.android.apidemo.R;
 
+import java.io.File;
 import java.security.Permission;
 
 public class Activity_Activity extends AppCompatActivity {
@@ -27,5 +35,27 @@ public class Activity_Activity extends AppCompatActivity {
                 Manifest.permission.ACCESS_FINE_LOCATION
         };
         requestPermissions(permissions, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (R.id.installNrfLogger == item.getItemId()) {
+            installNrfLogger(null);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresPermission("android.permission.REQUEST_INSTALL_PACKAGES")
+    public void installNrfLogger(View view){
+        String assetFilePath = "apk/nRFLogger.apk";
+        String destFilePath = new File(Utils.getApp().getExternalCacheDir(), "app.apk").toString();
+        ResourceUtils.copyFileFromAssets(assetFilePath, destFilePath);
+        AppUtils.installApp(destFilePath);
     }
 }
