a14ceb3ee469868dcfa5852506e5bc53652492d2
宋帮远 <songbangyuan@bbk.com>
2021-01-26 16:40:38 +0800

    nearby

---
+++ app/src/main/java/x/android/hardware/ConsumerIrManagerActivity.java
@@ -0,0 +1,16 @@
+package x.android.hardware;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bysong.android.apidemo.R;
+
+public class ConsumerIrManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consumer_ir_manager);
+    }
+}
