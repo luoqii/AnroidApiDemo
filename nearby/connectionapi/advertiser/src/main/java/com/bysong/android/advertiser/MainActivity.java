f09cf68a842fad444088fb3f445902dbf2226be8
宋帮远 <songbangyuan@bbk.com>
2021-04-06 15:02:16 +0800

    nearby demo

--- nearby/connectionapi/advertiser/src/main/java/com/bysong/android/advertiser/MainActivity.java
+++ nearby/connectionapi/advertiser/src/main/java/com/bysong/android/advertiser/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.nearby.Nearby;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private Object STRATEGY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void startAdvertising() {
+        AdvertisingOptions advertisingOptions =
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build();
+        Nearby.getConnectionsClient(this)
+                .startAdvertising(
+                        getUserNickname(), SERVICE_ID, connectionLifecycleCallback, advertisingOptions)
+                .addOnSuccessListener(
+                        (Void unused) -> {
+                            // We're advertising!
+                        })
+                .addOnFailureListener(
+                        (Exception e) -> {
+                            // We were unable to start advertising.
+                        });
     }
 }
